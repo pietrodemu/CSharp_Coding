ed975e4b561c981a5425c11f8fcb4fdc8023bd4b
pietrodemu <pietrodemu@hotmail.it>
2021-03-25 23:39:39 +0100

    Exercise 3.2: Prompt Parameter input

---
+++ src/Capitolo_3/PromptParameter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PromptParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // METODO INSERIMENTO PARAMETRI DA PROMPT:
+            var nome = args[0];
+            var cognome = args[1];
+            var data = DateTime.Today;
+            var ora = DateTime.Now;
+            Console.WriteLine($"Ciao {nome} {cognome}, oggi e' il giorno {data:d} e sono le ore {ora:HH:mm:ss}.");
+            Console.WriteLine("Premi un tasto per uscire");
+            Console.ReadKey();
+
+            // METODO CON INTERAZIONE UTENTE:
+            // Console.WriteLine("Nome: ");
+            // var nome = Console.ReadLine();
+            // Console.WriteLine("Cognome: ");
+            // var cognome = Console.ReadLine();
+            // var data = DateTime.Today;
+            // var ora = DateTime.Now;
+            // Console.WriteLine($"Ciao {nome} {cognome}, oggi e' il giorno {data:d} e sono le ore {ora:HH:mm:ss}.");
+            // Console.WriteLine("Premi un tasto per uscire");
+            // Console.ReadKey();
+        }
+    }
+}
