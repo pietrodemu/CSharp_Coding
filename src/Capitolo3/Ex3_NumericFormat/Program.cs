536eac44d50463552f65b9fba0437c571f655a46
pietrodemu <pietrodemu@hotmail.it>
2021-03-28 22:23:41 +0200

    Exercise 3.3: Numeric format type

---
+++ src/Capitolo3/Ex3_NumericFormat/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ex3_NumericFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 123.456;
+            Console.WriteLine($"Valuta: {number:C2}");
+            Console.WriteLine($"Due cifre decimali: {number:N2}");
+            Console.WriteLine($"Intero: {number:N0}");
+            Console.WriteLine("Percentuale una cifra decimale: {0:P1}", number/100);
+        }
+    }
+}
