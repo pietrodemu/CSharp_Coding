6abf43f20baaf4f39633c124c27ca38b8bfdfa85
pietrodemu <pietrodemu@hotmail.it>
2021-04-14 17:03:41 +0200

    Exercise 4.3: Input Array has been correctly implemented

--- src/Capitolo4/Ex3_InputArray/Program.cs
+++ src/Capitolo4/Ex3_InputArray/Program.cs
@@ -11,18 +11,29 @@ namespace Ex3_InputArray
             int[] input = new int[5];
             for(var i=0; i<input.Length; i++)
             {
-                Console.WriteLine($"Number {i+1}: ");
-                var strInput = Console.ReadLine();
-                input[i] = Convert.ToInt32(strInput);
-                if (input[i] < min)
-                    min = input[i];
-                if (input[i] > max)
-                    max = input[i];
-                sum += input[i];
+                // Recupero il valore
+                Console.Write($"Number {i+1}: ");
+                var intInput = int.Parse(Console.ReadLine());
+                input[i] = intInput;
+
+                // Operatori di confronto
+                if (i == 0) 
+                {
+                    min = intInput;
+                    max = intInput;
+                }
+                else
+                {
+                    if (intInput < min)
+                        min = intInput;
+                    if (intInput > max)
+                        max = intInput;
+                }
+                sum += intInput;
             }
             double avg = sum/(input.Length);
-            Console.WriteLine("min: ", min);
-            Console.WriteLine("max: ", max);
+            Console.WriteLine($"min: {min}");
+            Console.WriteLine($"max: {max}");
             Console.WriteLine($"avg: {avg:N2}");
         }
     }
