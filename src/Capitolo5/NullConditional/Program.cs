540a399c336ddf364b757dfc7c8e287d5a83c9cd
pietrodemu <pietrodemu@hotmail.it>
2021-04-27 23:20:33 +0200

    Chapter 5: Null Conditional operator

---
+++ src/Capitolo5/NullConditional/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NullConditional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Operatore null conditional "?."
+            string str = null; 
+            // int len = str.Length; // NullReferenceException
+            int? len = str?.Length;
+            System.Console.WriteLine($"lunghezza: {len}");
+        }
+    }
+}
